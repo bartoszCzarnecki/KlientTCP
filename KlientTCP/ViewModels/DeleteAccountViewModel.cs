7062679739e575a4319bd3f61b35b153e824cd4d
Olek <olekgalgan@gmail.com>
2020-12-06 14:08:44 +0100

    Add DeleteAccount view and viewmodel

--- KlientTCP/ViewModels/DeleteAccountViewModel.cs
+++ KlientTCP/ViewModels/DeleteAccountViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Caliburn.Micro;
+using KlientTCP.Events;
+using KlientTCP.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,28 @@ using System.Threading.Tasks;
 
 namespace KlientTCP.ViewModels
 {
-    class DeleteAccountViewModel
+    public class DeleteAccountViewModel : Screen
     {
+        private IAuthenticator _authenticator;
+        private IEventAggregator _aggregator;
+
+        public DeleteAccountViewModel(IAuthenticator authenticator, IEventAggregator aggregator)
+        {
+            _authenticator = authenticator;
+            _aggregator = aggregator;
+        }
+
+        public void HandleYesButtonClick()
+        {
+            if (_authenticator.DeleteAccount())
+            {
+                _aggregator.PublishOnUIThread(new LogoutEvent());
+            }
+        }
+
+        public void HandleNoButtonClick()
+        {
+
+        }
     }
 }
